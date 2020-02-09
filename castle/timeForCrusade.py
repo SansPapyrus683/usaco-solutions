badd0b7faa483df38ecb9503a886bd7370159e11
Kevin Sheng <kevinsheng2005@gmail.com>
2020-02-09 15:53:10 -0800

    have this

--- castle/timeForCrusade.py
+++ castle/timeForCrusade.py
@@ -3,12 +3,9 @@ ID: kevinsh4
 TASK: castle
 LANG: PYTHON3
 """
-from itertools import combinations
+from copy import deepcopy
+from sys import exit
 
-# 1: wall to the west
-# 2: wall to the north
-# 4: wall to the east
-# 8: wall to the south
 castle = []
 walls = {}
 with open('holyMusicStops.txt') as read:
@@ -28,19 +25,52 @@ with open('holyMusicStops.txt') as read:
                         walls[(v, x)].add((v, x + 1))  # wall between these two cells
                     elif w == 8:
                         walls[(v, x)].add((v + 1, x))  # zeroes mean they're on the border or smth like that
-
-print(castle)
-print(walls)
+        else:
+            maxWidth, maxHeight = [int(i) for i in row.rstrip().split()]
 
 
 def findRoomNeighbors(cell):
-    possibleNeighbors = {poss for poss in [(cell[0] - 1, cell[1]), (cell[0] + 1, cell[1]), (cell[0], cell[1] - 1), (cell[0], cell[1] + 1)] if poss not in walls[cell]}
-    return possibleNeighbors
+    poss = {(cell[0] - 1, cell[1]), (cell[0] + 1, cell[1]), (cell[0], cell[1] - 1), (cell[0], cell[1] + 1)}
+    cellWalls = []
+    killList = []
+    for p in poss:
+        if p in walls[cell]:
+            killList.append(p)
+            if p[0] not in (0, maxHeight + 1) and p[1] not in (0, maxWidth + 1):
+                cellWalls.append((cell, p))
+
+    for target in killList:
+        poss.remove(target)
+
+    return poss, cellWalls
 
 
-visited = {}
+visited = set()
+sizeList = []
+rooms = {}
 for mini in castle:
+    if mini in visited:
+        continue
     frontier = [mini]
-    for c in frontier:
-        for n in findRoomNeighbors(c):
-            pass
+    sizeCount = 1
+    roomWalls = set()
+    roomCells = set()
+    while frontier:
+        inLine = []
+        for c in frontier:
+            neighbors = findRoomNeighbors(c)
+            visited.add(c)
+            roomCells.add(c)
+            for wall in neighbors[1]:
+                roomWalls.add(wall)
+            for n in neighbors[0]:
+                if n not in visited:
+                    inLine.append(n)
+                    sizeCount += 1
+        frontier = inLine
+    print(sizeCount)
+    print(roomCells)
+    print(roomWalls)
+    sizeList.append(sizeCount)
+
+print(sizeList)
