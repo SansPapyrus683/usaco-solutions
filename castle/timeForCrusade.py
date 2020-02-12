b2dea7df61450bb9610ed9966513585860a678e0
Kevin Sheng <kevinsheng2005@gmail.com>
2020-02-11 16:11:16 -0800

    finally
    made a skeleton for what the darn program should look like
    watch it fail on test 2

--- castle/timeForCrusade.py
+++ castle/timeForCrusade.py
@@ -3,9 +3,10 @@ ID: kevinsh4
 TASK: castle
 LANG: PYTHON3
 """
-from copy import deepcopy
+from itertools import combinations
 from sys import exit
 
+written = open('outputs.txt', 'w')
 castle = []
 walls = {}
 with open('holyMusicStops.txt') as read:
@@ -36,8 +37,8 @@ def findRoomNeighbors(cell):
     for p in poss:
         if p in walls[cell]:
             killList.append(p)
-            if p[0] not in (0, maxHeight + 1) and p[1] not in (0, maxWidth + 1):
-                cellWalls.append((cell, p))
+            if p[0] not in (0, maxHeight + 1) and p[1] not in (0, maxWidth + 1):  # make sure it's not the border walls
+                cellWalls.append(tuple(sorted((cell, p))))
 
     for target in killList:
         poss.remove(target)
@@ -46,21 +47,18 @@ def findRoomNeighbors(cell):
 
 
 visited = set()
-sizeList = []
-rooms = {}
+roomList = []
 for mini in castle:
     if mini in visited:
         continue
     frontier = [mini]
     sizeCount = 1
     roomWalls = set()
-    roomCells = set()
     while frontier:
         inLine = []
         for c in frontier:
             neighbors = findRoomNeighbors(c)
             visited.add(c)
-            roomCells.add(c)
             for wall in neighbors[1]:
                 roomWalls.add(wall)
             for n in neighbors[0]:
@@ -68,9 +66,31 @@ for mini in castle:
                     inLine.append(n)
                     sizeCount += 1
         frontier = inLine
-    print(sizeCount)
-    print(roomCells)
-    print(roomWalls)
-    sizeList.append(sizeCount)
+    roomList.append([sizeCount, roomWalls])
 
-print(sizeList)
+written.write(str(len(roomList)) + '\n' + str(max(i[0] for i in roomList)) + '\n')
+
+combinedSizes = []
+for comb in combinations(roomList, 2):
+    currentPair = []
+    for w in comb[0][1]:
+        if w in comb[1][1]:
+            currentPair.append(w)
+    if currentPair:
+        currentPair.append(comb[0][0] + comb[1][0])
+        combinedSizes.append(currentPair)
+
+biggest = max(i[-1] for i in combinedSizes)
+written.write(str(biggest) + '\n')
+optimalWalls = []
+
+for bigRoom in combinedSizes:
+    if bigRoom[-1] == biggest:
+        optimalWalls.extend(bigRoom[:-1])
+
+for v, wall in enumerate(optimalWalls):  # now i just change it to be associated with cells instead of walls
+    optimalWalls[v] = [wall[0], 'E'] if wall[0][1] != wall[1][1] else [wall[1], 'N']  # left-right or up-down wall
+
+optimalWalls.sort()
+westest = [w for w in optimalWalls if w[0][0] == max([i[0][0] for i in optimalWalls])]
+written.write()
