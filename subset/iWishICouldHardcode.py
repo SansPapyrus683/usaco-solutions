6e9456067b0fa2c011fcf60cbd4c4f8260229bc8
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-02-20 12:40:57 -0800

    frick, too slow

--- subset/iWishICouldHardcode.py
+++ subset/iWishICouldHardcode.py
@@ -4,18 +4,23 @@ TASK: subset
 LANG: PYTHON3
 """
 from itertools import combinations
+from sys import exit
 
 with open('wellTooBad.txt') as read:
     upToNum = int(read.read().rstrip())
 
 actualSet = {i for i in range(1, upToNum + 1)}
 
+if sum(actualSet) % 2:
+    with open('outputs.txt', 'w') as written:
+        written.write('\n')
+        exit()
+
+checkSum = sum(actualSet)//2
 goodPartitionCount = 0
-for i in range(1, upToNum//2 + 1):
+for i in range(1, upToNum//2 + 1):  # only generate so many subsets
     for firstPart in combinations(actualSet, i):
-        firstPart = set(firstPart)
-        secondPart = actualSet - firstPart
-        if sum(secondPart) == sum(firstPart):
+        if sum(firstPart) == checkSum:
             goodPartitionCount += 1
 
 with open('outputs.txt', 'w') as written:
