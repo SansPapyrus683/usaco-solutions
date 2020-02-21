57aff2b83d349c60a288d82b3622c0a5af4906ca
Kevin Sheng <kevinsheng2005@gmail.com>
2020-02-20 16:58:16 -0800

    ok completed
    i totally didnt look it up or anything

--- subset/iWishICouldHardcode.py
+++ subset/iWishICouldHardcode.py
@@ -3,44 +3,29 @@ ID: kevinsh4
 TASK: subset
 LANG: PYTHON3
 """
-from itertools import combinations
-from sys import exit
+"""
+ok so this algo needs some 'splaining to do
+
+"""
 
 with open('wellTooBad.txt') as read:
     upToNum = int(read.read().rstrip())
 
 actualSet = {i for i in range(1, upToNum + 1)}
-checkSum = sum(actualSet)//2
 
-<<<<<<< HEAD
-sumNumbers = {1: [1, 2], 2: [1, 1, 1, 1]}  # 0, 1, 2, 3...
+sumNumbers = {1: [1, 1], 2: [1, 1, 1, 1]}  # 0, 1, 2, 3...
 needToAdd = 3
 while upToNum not in sumNumbers:
-    oldList = sumNumbers[needToAdd-1].copy()
-    for i in range(sum([x for x in range(needToAdd)])):
-        if i < needToAdd:
-            continue
-        if i < len(oldList):
-            oldList[i] += 1
-        else:
-            oldList.append(1)
+    oldList = sumNumbers[needToAdd - 1].copy()
+    addToOld = [0 for x in range(needToAdd)] + oldList
+    for v, i in enumerate(oldList):
+        addToOld[v] += i
+    sumNumbers[needToAdd] = addToOld
     needToAdd += 1
-=======
-if sum(actualSet) % 2:
-    with open('outputs.txt', 'w') as written:
-        written.write('\n')
-        exit()
 
-checkSum = sum(actualSet)//2
-goodPartitionCount = 0
-for i in range(1, upToNum//2 + 1):  # only generate so many subsets
-    for firstPart in combinations(actualSet, i):
-        if sum(firstPart) == checkSum:
-            goodPartitionCount += 1
->>>>>>> 6e9456067b0fa2c011fcf60cbd4c4f8260229bc8
-
+print(sumNumbers)
 with open('outputs.txt', 'w') as written:
     if sum(actualSet) % 2:
         written.write('0\n')
     else:
-        written.write(str(sumNumbers[upToNum][checkSum]))
+        written.write(str(sumNumbers[upToNum][sum(actualSet)//2]//2) + '\n')
