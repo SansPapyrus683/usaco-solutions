772a06820f95edd503bdaaf2b29c3512d4562bff
Kevin Sheng <kevinsheng2005@gmail.com>
2020-03-04 20:32:21 -0800

    sync

--- nocows/alabamaCows.py
+++ nocows/alabamaCows.py
@@ -3,10 +3,17 @@ ID: kevinsh4
 TASK: nocows
 LANG: PYTHON3
 """
+from collections import defaultdict
+
 with open('banjos.txt') as read:
     cowNum, genNum = [int(c) for c in read.read().rstrip().split()]
 
-def cowNeighbors(gen, cowNum):  # like array indexes- stats at 0 and all that
-    return [gen + 1, cowNum]
+treeTable = defaultdict(lambda: defaultdict(lambda: 0))  # the table[i][j] is # of trees of depth i and j cows
+shallowerTrees = defaultdict(lambda: defaultdict(lambda: 0))  # the table[i][j] still has j cows, but all's depth <= i
+treeTable[1][1] = 1
 
-pedigree = [[False] * 2**i for i in range(genNum)]  # no cow in the gen space = False, cow = True
+for i in range(2, genNum + 1):  # we'll find the depths of the trees one at a time
+    print('doing gen %i' % i)
+    for cows in range(1, sum([2**x for x in range(i)]) + 1, 2):
+        print(cows)
+    print('-----')
