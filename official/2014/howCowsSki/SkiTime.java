fbe4f46ab25851cc450fffe38271761227a120e3
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-10-30 12:07:49 -0700

    a bit faster

--- official/2014/howCowsSki/SkiTime.java
+++ official/2014/howCowsSki/SkiTime.java
@@ -4,46 +4,53 @@ import java.util.stream.Stream;
 
 // 2014 jan silver i think (if i name this ccski intellij has a seizure)
 public class SkiTime {
-    static int[] dimensions;
+    static int width, length;
     static int[][] hills;
     static int checkpointNum = 0;
     static boolean[][] checkpoints;
-    static int[] randomCheckpoint;
-    static ArrayList<int[]>[][] neighbors;
-    // static long[] times = new long[5];
+    static Point randomCheckpoint;
+    static long asdf = 0;
+
+    static class Point {
+        public int x;
+        public int y;
+        public Point(int y, int x) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
         BufferedReader read = new BufferedReader(new FileReader("ccski.in"));
-        dimensions = Stream.of(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        hills = new int[dimensions[0]][dimensions[1]];
+        String[] dimensions = read.readLine().split(" ");
+        width = Integer.parseInt(dimensions[0]);
+        length = Integer.parseInt(dimensions[1]);
+        hills = new int[width][length];
 
-        neighbors = new ArrayList[dimensions[0]][dimensions[1]];  // precalculate neighbors
-        for (int y = 0; y < dimensions[0]; y++) {
-            for (int x = 0; x < dimensions[1]; x++) {
-                neighbors[y][x] = neighbors(y, x);
+        for (int i = 0; i < hills.length; i++) {
+            StringTokenizer row = new StringTokenizer(read.readLine());
+            for (int c = 0; c < length; c++) {
+                hills[i][c] = Integer.parseInt(row.nextToken());
             }
         }
 
+        checkpoints = new boolean[width][length];
         for (int i = 0; i < hills.length; i++) {
-            hills[i] = Stream.of(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-
-        checkpoints = new boolean[dimensions[0]][dimensions[1]];
-        for (int i = 0; i < hills.length; i++) {
-            int xVal = 0;
-            for (String rawPt : read.readLine().split(" ")) {
-                int pt = Integer.parseInt(rawPt);
-                if (pt == 1) {
-                    randomCheckpoint = new int[] {i, xVal};
-                    checkpoints[i][xVal] = true;
+            StringTokenizer row = new StringTokenizer(read.readLine());
+            for (int c = 0; c < length; c++) {
+                checkpoints[i][c] = Integer.parseInt(row.nextToken()) == 1;
+                if (checkpoints[i][c]) {
+                    if (randomCheckpoint == null) {
+                        randomCheckpoint = new Point(i, c);
+                    }
                     checkpointNum++;
                 }
-                xVal++;
             }
         }
 
         int lowerBound = 0;
-        int upperBound = (int) Math.pow(10, 9) + 1;  // not inclusive (but the lower bound is)
+        int upperBound = (int) Math.pow(10, 9);
         while (upperBound > lowerBound) {
             int toSearch = (upperBound + lowerBound) / 2;
             if (validWithDifficulty(toSearch)) {
@@ -52,49 +59,53 @@ public class SkiTime {
                 lowerBound = toSearch + 1;
             }
         }
+        // binary search do be like this tho
+        int answer = validWithDifficulty(lowerBound) ? lowerBound : lowerBound + 1;
+
         PrintWriter written = new PrintWriter(new FileOutputStream("ccski.out"));
-        written.println(lowerBound);  // or upperBound - 1, both work
+        written.println(answer);  // or upperBound - 1, both work
         written.close();
-        System.out.println(lowerBound);
+        System.out.println(answer);
+        System.out.println(asdf / Math.pow(10, 9));
         System.out.printf("grand total of %d ms ok buddy%n", System.currentTimeMillis() - start);
     }
 
-    static ArrayList<int[]> neighbors(int c1, int c2) {
-        int[][] possNeighbors = new int[][]{
-                {c1 + 1, c2}, {c1 - 1, c2}, {c1, c2 + 1}, {c1, c2 - 1}
-        };
-        ArrayList<int[]> actualNeighbors = new ArrayList<>();
-        for (int[] n : possNeighbors) {
-            if (0 <= n[0] && n[0] < dimensions[0] && 0 <= n[1] && n[1] < dimensions[1]) {
-                actualNeighbors.add(n);
-            }
-        }
-        return actualNeighbors;
-    }
-
     static boolean validWithDifficulty(int diff) {
-        int[] start = randomCheckpoint;  // doesn't matter which cp we use honestly
-        int leftToCheck = checkpointNum;  // we only go through each cp once so just a number is good enough
-        ArrayDeque<int[]> frontier = new ArrayDeque<>();
-        boolean[][] visited = new boolean[dimensions[0]][dimensions[1]];
-        frontier.add(start);
-        visited[start[0]][start[1]] = true;
+        Point current = randomCheckpoint;  // doesn't matter which cp we use honestly
+        int gottenCheckpoints = 0;
+        ArrayDeque<Point> frontier = new ArrayDeque<>();
+        boolean[][] visited = new boolean[width][length];
+        frontier.add(current);
+        visited[current.y][current.x] = true;
+
         while (!frontier.isEmpty()) {
-            int[] current = frontier.poll();
-            if (checkpoints[current[0]][current[1]]) {
-                leftToCheck--;
-                if (leftToCheck == 0) {
-                    return true;
-                }
+            current = frontier.pop();
+            int x = current.x;
+            int y = current.y;
+            if (checkpoints[y][x]) {
+                gottenCheckpoints++;
             }
-            int currElevation = hills[current[0]][current[1]];
-            for (int[] n : neighbors[current[0]][current[1]]) {
-                if (Math.abs(currElevation - hills[n[0]][n[1]]) <= diff && !visited[n[0]][n[1]]) {
-                    visited[n[0]][n[1]] = true;
-                    frontier.add(n);
-                }
+            /* yes, this is a really hacky implementation.
+             yes, i am absolutely ashamed of such blasphemy.
+             but it runs under the limit, so here it stays. */
+            int currElevation = hills[y][x];
+            if (x + 1 < length && Math.abs(currElevation - hills[y][x+1]) <= diff && !visited[y][x+1]) {
+                visited[y][x+1] = true;
+                frontier.add(new Point(y, x + 1));
+            }
+            if (x - 1 >= 0 && Math.abs(currElevation - hills[y][x-1]) <= diff && !visited[y][x-1]) {
+                visited[y][x-1] = true;
+                frontier.add(new Point(y, x- 1));
+            }
+            if (y + 1 < width && Math.abs(currElevation - hills[y+1][x]) <= diff && !visited[y+1][x]) {
+                visited[y+1][x] = true;
+                frontier.add(new Point(y + 1, x));
+            }
+            if (y - 1 >= 0 && Math.abs(currElevation - hills[y-1][x]) <= diff && !visited[y-1][x]) {
+                visited[y-1][x] = true;
+                frontier.add(new Point(y - 1, x));
             }
         }
-        return leftToCheck == 0;  // handles the 1 checkpoint edge case
+        return gottenCheckpoints >= checkpointNum;  // handles the 1 checkpoint edge case
     }
 }
