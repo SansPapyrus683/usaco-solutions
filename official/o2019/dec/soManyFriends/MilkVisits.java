8a318887a67f46968a351909d136f657811d36b3
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-11-10 10:38:51 -0800

    switched milkvisits to bufferedreader

--- official/o2019/dec/soManyFriends/MilkVisits.java
+++ official/o2019/dec/soManyFriends/MilkVisits.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Stream;
 
 // 2019 gold december (it took two whole weeks lol)
 public class MilkVisits {
@@ -17,27 +18,25 @@ public class MilkVisits {
 
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-        VisitingReader read = new VisitingReader("milkvisits.in");
-
-        farmNum = read.nextInt();
-        friendNum = read.nextInt();
-        types = new int[farmNum];
-        for (int i = 0; i < farmNum; i++) {
-            types[i] = read.nextInt();
-        }
+        BufferedReader read = new BufferedReader(new FileReader("milkvisits.in"));
+        StringTokenizer initial = new StringTokenizer(read.readLine());
+        farmNum = Integer.parseInt(initial.nextToken());
+        friendNum = Integer.parseInt(initial.nextToken());
+        types = Stream.of(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         happiness = new char[friendNum];
         Arrays.fill(happiness, '0');
         adj = new ArrayList[farmNum + 1];
-        for (int i = 1; i < farmNum + 1; i++) {  // we won't use index 0 because screw that
+        for (int i = 1; i < farmNum + 1; i++) {  // we won't use index 0 because the farms start @ 1
             adj[i] = new ArrayList<>();
         }
         inTimes = new int[farmNum + 1];  // still, we won't use the first index lol
         outTimes = new int[farmNum + 1];
 
         for (int i = 0; i < farmNum - 1; i++) {
-            int firstFarm = read.nextInt();
-            int secondFarm = read.nextInt();
+            StringTokenizer path = new StringTokenizer(read.readLine());
+            int firstFarm = Integer.parseInt(path.nextToken());
+            int secondFarm = Integer.parseInt(path.nextToken());
             adj[firstFarm].add(secondFarm);
             adj[secondFarm].add(firstFarm);
         }
@@ -48,10 +47,13 @@ public class MilkVisits {
         }
         queryLookup = new int[friendNum][2];
         for (int fn = 0; fn < friendNum; fn++) {  // farmer john has more friends than i ever will
-            int[] query = new int[] {read.nextInt(), read.nextInt(), read.nextInt()};
-            nodeQueries[query[0]].add(new int[] {fn, query[2]});
-            nodeQueries[query[1]].add(new int[] {fn, query[2]});
-            queryLookup[fn] = new int[] {query[0], query[1]};
+            StringTokenizer query = new StringTokenizer(read.readLine());
+            int from = Integer.parseInt(query.nextToken());
+            int to = Integer.parseInt(query.nextToken());
+            int milk = Integer.parseInt(query.nextToken());
+            nodeQueries[from].add(new int[] {fn, milk});
+            nodeQueries[to].add(new int[] {fn, milk});
+            queryLookup[fn] = new int[] {from, to};
         }
         traverseRecordTimes(1);
 
@@ -60,7 +62,6 @@ public class MilkVisits {
         written.printf("%s%n", new String(happiness));
         written.close();
         System.out.println(new String(happiness));
-
         System.out.printf("so we finished: it took something like %s milliseconds %n", System.currentTimeMillis() - start);
     }
 
@@ -95,7 +96,7 @@ public class MilkVisits {
 
     static void actualSol(int startNode) {
         HashSet<Integer> traversed = new HashSet<>();
-        LinkedList<int[]> frontier = new LinkedList<>();  // use it for its queue capability
+        LinkedList<int[]> frontier = new LinkedList<>();
         frontier.add(new int[] {startNode, 0});
 
         int[] stack = new int[(int) farmNum];
@@ -125,7 +126,7 @@ public class MilkVisits {
                     long haveGotten = theGot.get(theGot.size() - 1);  // see the seenTypes.get()... above for info
                     int otherFarm = queryLookup[qNum][0] == current ? queryLookup[qNum][1] : queryLookup[qNum][0];
                     // check if the below one is the other's ancestor- if it isn't, then we'll def have to visit that node on the way to the other one
-                    if (haveGotten / MAX_DEPTH == current || !fatherTest((Integer) stack[(int) (haveGotten % MAX_DEPTH + 1)], otherFarm)) {
+                    if (haveGotten / MAX_DEPTH == current || !fatherTest(stack[(int) (haveGotten % MAX_DEPTH + 1)], otherFarm)) {
                         happiness[qNum] = '1';
                     }
                 }
@@ -139,53 +140,5 @@ public class MilkVisits {
             }
             lastDepth = depth;
         }
-    }
-}
-
-class VisitingReader {
-    final private int BUFFER_SIZE = 1 << 16;
-    private DataInputStream din;
-    private byte[] buffer;
-    private int bufferPointer, bytesRead;
-
-    public VisitingReader(String file_name) throws IOException {
-        din = new DataInputStream(new FileInputStream(file_name));
-        buffer = new byte[BUFFER_SIZE];
-        bufferPointer = bytesRead = 0;
-    }
-
-    public int nextInt() throws IOException {
-        int ret = 0;
-        byte c = read();
-        while (c <= ' ')
-            c = read();
-        boolean neg = (c == '-');
-        if (neg)
-            c = read();
-        do {
-            ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
-
-        if (neg)
-            return -ret;
-        return ret;
-    }
-
-    private void fillBuffer() throws IOException {
-        bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-        if (bytesRead == -1)
-            buffer[0] = -1;
-    }
-
-    private byte read() throws IOException {
-        if (bufferPointer == bytesRead)
-            fillBuffer();
-        return buffer[bufferPointer++];
-    }
-
-    public void close() throws IOException {
-        if (din == null)
-            return;
-        din.close();
     }
 }
