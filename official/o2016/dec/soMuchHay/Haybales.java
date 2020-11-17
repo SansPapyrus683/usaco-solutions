8a21769ee522aca4a0dee862a3a666ec7e84bfb8
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-11-16 21:57:21 -0800

    haybales ez clap

---
+++ official/o2016/dec/soMuchHay/Haybales.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+// 2016 dec silver
+public class Haybales {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        BufferedReader read = new BufferedReader(new FileReader("haybales.in"));
+        StringTokenizer initial = new StringTokenizer(read.readLine());
+        initial.nextToken();
+        int[] haybales = Arrays.stream(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(haybales);
+
+        int queryNum = Integer.parseInt(initial.nextToken());
+        PrintWriter written = new PrintWriter("haybales.out");
+        for (int q = 0; q < queryNum; q++) {
+            StringTokenizer query = new StringTokenizer(read.readLine());
+            int qStart = Integer.parseInt(query.nextToken());
+            int qEnd = Integer.parseInt(query.nextToken());
+            // find the insert location of the intervals relative to the bales and then subtract them
+            int haybaleNum = bisect_right(haybales, qEnd) - bisect_left(haybales, qStart);
+            written.println(haybaleNum);
+        }
+        written.close();
+        System.out.printf("bruh %d ms you're so bad%n", System.currentTimeMillis() - start);
+    }
+
+    // straight from https://stackoverflow.com/questions/2945017/javas-equivalent-to-bisect-in-python
+    public static int bisect_right(int[] searchOn, int x) {
+        int lo = 0;
+        int hi = searchOn.length;
+        if (hi == 0) {
+            return 0;
+        }
+        if (x < searchOn[lo]) {
+            return lo;
+        }
+        if (x > searchOn[hi - 1]) {
+            return hi;
+        }
+        while (true) {
+            if (lo + 1 == hi) {
+                return lo + 1;
+            }
+            int mi = (hi + lo) / 2;
+            if (x < searchOn[mi]) {
+                hi = mi;
+            } else {
+                lo = mi;
+            }
+        }
+    }
+
+    public static int bisect_left(int[] searchOn, int x) {
+        int lo = 0;
+        int hi = searchOn.length;
+        if (hi == 0) {
+            return 0;
+        }
+        if (x < searchOn[lo]) {
+            return lo;
+        }
+        if (x > searchOn[hi - 1]) {
+            return hi;
+        }
+        while (true) {
+            if (lo + 1 == hi) {
+                return x == searchOn[lo] ? lo : (lo + 1);
+            }
+            int mi = (hi + lo) / 2;
+            if (x <= searchOn[mi]) {
+                hi = mi;
+            } else {
+                lo = mi;
+            }
+        }
+    }
+}
--- section3/part3/range/westVirginia.py
+++ section3/part3/range/westVirginia.py
@@ -2,6 +2,7 @@
 ID: kevinsh4
 LANG: PYTHON3
 TASK: range
+yeah this is too slow lol
 """
 
 with open('range.in') as read:
@@ -9,21 +10,37 @@ with open('range.in') as read:
 		if v == 0:
 			grass = [[] for _ in range(int(l))]
 		else:
-			grass[v-1] = [int(i) for i in l[:-1]]
+			grass[v-1] = [int(i) for i in l.rstrip()]
 fieldWidth = len(grass)
 calcedBefore = set()
-squareSizes = {s: 0 for s in range(1, fieldWidth + 1)}
+squareSizes = {s: set() for s in range(1, fieldWidth + 1)}
 
 
-def hugeUpdateSmall(squareSize):
-	for i in range(1, squareSize + 1):
-		pass
+def newSquares(squareSize, r, c):
+	for i in range(2, squareSize + 1):
+		for a in range(squareSize - i + 1):
+			for b in range(squareSize - i + 1):
+				squareSizes[i].add((r + a, c + b))
 
 
-for w in range(fieldWidth, 0, -1):
-	pass
+for w in range(fieldWidth, 1, -1):
+	for x in range(0, fieldWidth - w + 1):  # the starting x and y
+		for y in range(0, fieldWidth - w + 1):
+			if (x, y) in squareSizes[w]:
+				continue
+			valid = True
+			for i in range(x, x + w):
+				for j in range(y, y + w):
+					if not grass[i][j]:
+						valid = False
+						break
+				if not valid:
+					break
+			else:
+				newSquares(w, x, y)
 
 with open('range.out', 'w') as written:
 	for s in squareSizes:
-		if squareSizes[s] > 0:
-			written.write(f'{s} {squareSizes[s]}\n')
+		if len(squareSizes[s]) > 0:
+			print(f'{s} {len(squareSizes[s])}')
+			written.write(f'{s} {len(squareSizes[s])}\n')
