d31aa7a2bd11e1d873a2cee0f8aefeafa2fcc98e
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-11-18 17:43:52 -0800

    did range

---
+++ section3/part3/range/WestVirginia.java
@@ -0,0 +1,58 @@
+/*
+ID: kevinsh4
+LANG: JAVA
+TASK: range
+*/
+
+import java.io.*;
+
+// pretty much a direct java translation
+public class WestVirginia {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        BufferedReader read = new BufferedReader(new FileReader("range.in"));
+        int fieldWidth = Integer.parseInt(read.readLine());
+        int[][] grass = new int[fieldWidth][fieldWidth];
+        for (int i = 0; i < fieldWidth; i++) {
+            char[] row = read.readLine().toCharArray();
+            int[] inputTo = grass[i];
+            for (int c = 0; c < fieldWidth; c++) {
+                inputTo[c] = Character.getNumericValue(row[c]);
+            }
+        }
+
+        int[][] grassSoFar = new int[fieldWidth + 1][fieldWidth + 1];
+        grassSoFar[1][1] = grass[0][0];
+        for (int i = 1; i < fieldWidth + 1; i++) {
+            grassSoFar[1][i] = grassSoFar[1][i - 1] + grass[0][i - 1];
+            grassSoFar[i][1] = grassSoFar[i - 1][1] + grass[i - 1][0];
+        }
+        for (int r = 2; r < fieldWidth + 1; r++) {
+            for (int c = 2; c < fieldWidth + 1; c++) {
+                grassSoFar[r][c] = (grassSoFar[r - 1][c] +
+                                    grassSoFar[r][c - 1] -
+                                    grassSoFar[r - 1][c - 1] +
+                                    grass[r - 1][c - 1]);
+            }
+        }
+
+        PrintWriter written = new PrintWriter("range.out");
+        for (int w = 2; w <= fieldWidth; w++) {
+            int targetSum = (int) Math.pow(w, 2);
+            int validNumber = 0;
+            for (int r = 0; r <= fieldWidth - w; r++) {
+                for (int c = 0; c <= fieldWidth - w; c++) {
+                    if ((grassSoFar[r + w][c + w] - grassSoFar[r][c + w] - grassSoFar[r + w][c] + grassSoFar[r][c]) == targetSum) {
+                        validNumber++;
+                    }
+                }
+            }
+            if (validNumber > 0) {
+                System.out.printf("%s %s%n", w, validNumber);
+                written.printf("%s %s%n", w, validNumber);
+            }
+        }
+        written.close();
+        System.out.printf("MAMAAAAAAAA WEST VIRGINIAAAAAAAAAA %d ms%n", System.currentTimeMillis() - start);
+    }
+}
--- section3/part3/range/westVirginia.py
+++ section3/part3/range/westVirginia.py
@@ -2,45 +2,42 @@
 ID: kevinsh4
 LANG: PYTHON3
 TASK: range
-yeah this is too slow lol
+yeah this is too slow lol, but i'm going to leave it here
 """
 
 with open('range.in') as read:
-	for v, l in enumerate(read):
-		if v == 0:
-			grass = [[] for _ in range(int(l))]
-		else:
-			grass[v-1] = [int(i) for i in l.rstrip()]
-fieldWidth = len(grass)
-calcedBefore = set()
-squareSizes = {s: set() for s in range(1, fieldWidth + 1)}
+    for v, l in enumerate(read):
+        if v == 0:
+            fieldWidth = int(l)
+            grass = [[] for _ in range(int(l))]
+        else:
+            grass[v - 1] = [int(i) for i in l.rstrip()]
 
+grassSoFar = [[0 for _ in range(fieldWidth + 1)] for _ in range(fieldWidth + 1)]
+squareSizes = {s: 0 for s in range(2, fieldWidth + 1)}
 
-def newSquares(squareSize, r, c):
-	for i in range(2, squareSize + 1):
-		for a in range(squareSize - i + 1):
-			for b in range(squareSize - i + 1):
-				squareSizes[i].add((r + a, c + b))
+# so this uses 2d prefix sums (i'm too lazy to explain it, just look it up)
+# it basically sums submatrices in O(1) time, which optimizes it enough so it's probably fast enough
+grassSoFar[1][1] = grass[0][0]
+for i in range(1, fieldWidth + 1):
+    grassSoFar[1][i] = grassSoFar[1][i - 1] + grass[0][i - 1]
+    grassSoFar[i][1] = grassSoFar[i - 1][1] + grass[i - 1][0]
+for r in range(2, fieldWidth + 1):
+    for c in range(2, fieldWidth + 1):
+        grassSoFar[r][c] = (grassSoFar[r - 1][c] +
+                            grassSoFar[r][c - 1] -
+                            grassSoFar[r - 1][c - 1] +
+                            grass[r - 1][c - 1])
 
-
-for w in range(fieldWidth, 1, -1):
-	for x in range(0, fieldWidth - w + 1):  # the starting x and y
-		for y in range(0, fieldWidth - w + 1):
-			if (x, y) in squareSizes[w]:
-				continue
-			valid = True
-			for i in range(x, x + w):
-				for j in range(y, y + w):
-					if not grass[i][j]:
-						valid = False
-						break
-				if not valid:
-					break
-			else:
-				newSquares(w, x, y)
-
-with open('range.out', 'w') as written:
-	for s in squareSizes:
-		if len(squareSizes[s]) > 0:
-			print(f'{s} {len(squareSizes[s])}')
-			written.write(f'{s} {len(squareSizes[s])}\n')
+written = open('range.out', 'w')
+for w in range(2, fieldWidth + 1):
+    targetSum = w ** 2
+    numberValid = 0
+    for r in range(fieldWidth - w + 1):
+        for c in range(fieldWidth - w + 1):
+            if (grassSoFar[r + w][c + w] - grassSoFar[r][c + w] - grassSoFar[r + w][c] + grassSoFar[r][c]) == targetSum:
+                numberValid += 1
+    if numberValid:
+        print(f'{w} {numberValid}')
+        written.write(f'{w} {numberValid}\n')
+written.close()
