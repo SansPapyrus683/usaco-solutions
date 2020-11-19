5deee7ef62912e7c2c541cad51d43994638f3ec3
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-11-18 21:29:34 -0800

    did game1 and leftout

---
+++ official/o2019/usopen/bruhPhotography/LeftOut.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+// 2019 usopen silver
+public class LeftOut {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        BufferedReader read = new BufferedReader(new FileReader("leftout.in"));
+        int side = Integer.parseInt(read.readLine());
+        // with 1000x1000, we don't need to use arrays lol (nor CAN we i think)
+        HashMap<ArrayList<Boolean>, Integer> rowOccurrences = new HashMap<>();
+        ArrayList<Boolean>[] cows = new ArrayList[side];
+        for (int i = 0; i < side; i++) {
+            char[] row = read.readLine().toCharArray();
+            ArrayList<Boolean> actualRow = new ArrayList<>();
+            for (char c : row) {
+                actualRow.add(c == 'R');
+            }
+            rowOccurrences.put(actualRow, rowOccurrences.getOrDefault(actualRow, 0) + 1);
+            cows[i] = actualRow;
+        }
+
+        /*
+         * logic:
+         * ok so the only way we can accomplish the photo by shouting is if all the rows are "inversions" of each other
+         * (think it'd work for columns too but whatever) and by inversions i mean you can go from any row to any
+         * other row by inverting it, because then you can make them all the same, then invert the columns
+         */
+        String leftOut = "-1";
+        if (rowOccurrences.size() != 3) {  // if != 3, it's either already possible or not possible even if we change a cow
+            assert true;
+        } else {
+            int rowAt = 1;  // the answer is by 1-based indexing, might as well
+            searching:
+            for (ArrayList<Boolean> row : cows) {  // find the cow by going through rows, then by columns
+                if (rowOccurrences.get(row) == 1) {
+                    ArrayList<Boolean> tryingRow = new ArrayList<>(row);
+                    for (int i = 0; i < side; i++) {
+                        tryingRow.set(i, !tryingRow.get(i));
+                        if (rowOccurrences.containsKey(tryingRow)) {
+                            leftOut = String.format("%s %s", rowAt, i + 1);
+                            break searching;
+                        }
+                        tryingRow.set(i, !tryingRow.get(i));  // revert the change
+                    }
+                }
+                rowAt++;
+            }
+        }
+
+        PrintWriter written = new PrintWriter("leftout.out");
+        written.println(leftOut);
+        written.close();
+        System.out.println(leftOut);
+        System.out.printf("%d ms. need i say more?%n", System.currentTimeMillis() - start);
+    }
+}
---
+++ section3/part3/game1/Numbergasms.java
@@ -0,0 +1,56 @@
+/*
+ID: kevinsh4
+LANG: JAVA
+TASK: game1
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Numbergasms {
+    private static int[][] cachedFromTos;
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        BufferedReader read = new BufferedReader(new FileReader("game1.in"));
+        read.readLine();
+        StringBuilder allNums = new StringBuilder();
+        String rnLine;
+        while ((rnLine = read.readLine()) != null) {
+            allNums.append(rnLine);
+            allNums.append(" ");
+        }
+        int[] numbers = Arrays.stream(allNums.toString().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int player1 = p1BestScore(numbers);
+        int totalScore = Arrays.stream(numbers).sum();
+        PrintWriter written = new PrintWriter("game1.out");
+        written.printf("%s %s%n", player1, totalScore - player1);
+        written.close();
+        System.out.printf("%s %s%n", player1, totalScore - player1);
+        System.out.printf("%d ms! ok... well imma go now%n", System.currentTimeMillis() - start);
+    }
+
+    static int p1BestScore(int[] board) {
+        cachedFromTos = new int[board.length][board.length];
+        for (int i = 0; i < board.length; i++) {
+            Arrays.fill(cachedFromTos[i], -1);
+        }
+        return p1BestScore(board, 0, board.length - 1);
+    }
+
+    // gives the best score with the board (and it's p1's turn) and only pieces from board[from: to + 1] are there
+    private static int p1BestScore(int[] board, int from, int to) {
+        if (cachedFromTos[from][to] != -1) {
+            return cachedFromTos[from][to];
+        }
+        if (to - from <= 1) {
+            return Math.max(board[from], board[to]);
+        }
+        cachedFromTos[from + 2][to] = p1BestScore(board, from + 2, to);
+        cachedFromTos[from + 1][to - 1] = p1BestScore(board, from + 1, to - 1);
+        cachedFromTos[from][to - 2] = p1BestScore(board, from, to - 2);
+        // it's math.min because p2 will always try to screw up p1, so we assume worst case
+        return Math.max(board[from] + Math.min(cachedFromTos[from + 2][to], cachedFromTos[from + 1][to - 1]),
+                board[to] + Math.min(cachedFromTos[from][to - 2], cachedFromTos[from + 1][to - 1]));
+    }
+}
