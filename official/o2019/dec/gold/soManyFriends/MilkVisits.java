1917b30fcff453bac41e7f796ccb956c3dd77de1
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-11-27 19:13:42 -0800

    fixed up 2019 dec

--- official/o2019/dec/gold/soManyFriends/MilkVisits.java
+++ official/o2019/dec/gold/soManyFriends/MilkVisits.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -6,102 +7,74 @@ import java.util.stream.Stream;
 // and even then this is rife of bad practices but it works so screw it
 public class MilkVisits {
     private static final long MAX_DEPTH = 100000;
-    static int farmNum;
-    static int friendNum;
-    static int[] types;
-    static ArrayList<Integer>[] adj;
-    static int[] inTimes;
-    static int[] outTimes;
-    static ArrayList<int[]>[] nodeQueries;
-    static int[][] queryLookup;
-    static char[] happiness;
 
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
         BufferedReader read = new BufferedReader(new FileReader("milkvisits.in"));
         StringTokenizer initial = new StringTokenizer(read.readLine());
-        farmNum = Integer.parseInt(initial.nextToken());
-        friendNum = Integer.parseInt(initial.nextToken());
-        types = Stream.of(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int farmNum = Integer.parseInt(initial.nextToken());
+        int friendNum = Integer.parseInt(initial.nextToken());
+        int[] types = Stream.of(read.readLine().split(" ")).mapToInt(m -> Integer.parseInt(m) - 1).toArray();
 
-        happiness = new char[friendNum];
+        char[] happiness = new char[friendNum];
         Arrays.fill(happiness, '0');
-        adj = new ArrayList[farmNum + 1];
-        for (int i = 1; i < farmNum + 1; i++) {  // we won't use index 0 because the farms start @ 1
+        ArrayList<Integer>[] adj = new ArrayList[farmNum];
+        for (int i = 0; i < farmNum; i++) {
             adj[i] = new ArrayList<>();
         }
-        inTimes = new int[farmNum + 1];  // still, we won't use the first index lol
-        outTimes = new int[farmNum + 1];
 
         for (int i = 0; i < farmNum - 1; i++) {
             StringTokenizer path = new StringTokenizer(read.readLine());
-            int firstFarm = Integer.parseInt(path.nextToken());
-            int secondFarm = Integer.parseInt(path.nextToken());
+            int firstFarm = Integer.parseInt(path.nextToken()) - 1;  // farms start at 1, let's make that 0
+            int secondFarm = Integer.parseInt(path.nextToken()) - 1;
             adj[firstFarm].add(secondFarm);
             adj[secondFarm].add(firstFarm);
         }
 
-        nodeQueries = new ArrayList[farmNum + 1];
-        for (int i = 1; i < farmNum + 1; i++) {
+        ArrayList<int[]>[] nodeQueries = new ArrayList[farmNum];
+        for (int i = 0; i < farmNum; i++) {
             nodeQueries[i] = new ArrayList<>();
         }
-        queryLookup = new int[friendNum][2];
-        for (int fn = 0; fn < friendNum; fn++) {  // farmer john has more friends than i ever will
+        int[][] queryLookup = new int[friendNum][2];
+        for (int f = 0; f < friendNum; f++) {  // farmer john has more friends than i ever will
             StringTokenizer query = new StringTokenizer(read.readLine());
-            int from = Integer.parseInt(query.nextToken());
-            int to = Integer.parseInt(query.nextToken());
-            int milk = Integer.parseInt(query.nextToken());
-            nodeQueries[from].add(new int[] {fn, milk});
-            nodeQueries[to].add(new int[] {fn, milk});
-            queryLookup[fn] = new int[] {from, to};
+            int from = Integer.parseInt(query.nextToken()) - 1;
+            int to = Integer.parseInt(query.nextToken()) - 1;
+            int milk = Integer.parseInt(query.nextToken()) - 1;  // so the types start at 1 too lol
+            nodeQueries[from].add(new int[] {f, milk});
+            nodeQueries[to].add(new int[] {f, milk});
+            queryLookup[f] = new int[] {from, to};
         }
-        traverseRecordTimes(1);
 
-        actualSol(1);
-        PrintWriter written = new PrintWriter("milkvisits.out");
-        written.printf("%s%n", new String(happiness));
-        written.close();
-        System.out.println(new String(happiness));
-        System.out.printf("so we finished: it took something like %s milliseconds %n", System.currentTimeMillis() - start);
-    }
-
-    private static void traverseRecordTimes(int startNode) {
+        boolean[] traversed = new boolean[farmNum];
+        int[] inTimes = new int[farmNum];
+        int[] outTimes = new int[farmNum];
         int movesTaken = 0;
-        HashSet<Integer> traversed = new HashSet<>();
-        LinkedList<Integer> frontier = new LinkedList<>();  // use it for its queue capability
-        frontier.add(startNode);
-
-        while (!frontier.isEmpty()) {
-            int current = frontier.removeFirst();
-            if (traversed.contains(current)) {  // this should be that marker i think
-                outTimes[current] = movesTaken;
+        ArrayDeque<Integer> timeFrontier = new ArrayDeque<>(Collections.singletonList(0));  // start @ node 0
+        while (!timeFrontier.isEmpty()) {
+            int curr = timeFrontier.removeFirst();
+            if (traversed[curr]) {  // this should be that marker i think
+                outTimes[curr] = movesTaken;
                 continue;
             }
 
-            inTimes[current] = movesTaken;
-            frontier.addFirst(current);  // set a marker to record the outtime
-            traversed.add(current);
-            for (int c : adj[current]) {
-                if (!traversed.contains(c)) {
-                    frontier.addFirst(c);
+            inTimes[curr] = movesTaken;
+            timeFrontier.addFirst(curr);  // set a marker to record the outtime
+            traversed[curr] = true;
+            for (int c : adj[curr]) {
+                if (!traversed[c]) {
+                    timeFrontier.addFirst(c);
                 }
             }
             movesTaken++;
         }
-    }
 
-    private static boolean fatherTest(int possDad, int possKid) {
-        return inTimes[possDad] <= inTimes[possKid] && outTimes[possDad] >= outTimes[possKid];
-    }
-
-    private static void actualSol(int startNode) {
-        HashSet<Integer> traversed = new HashSet<>();
-        LinkedList<int[]> frontier = new LinkedList<>();
-        frontier.add(new int[] {startNode, 0});
+        Arrays.fill(traversed, false);
+        ArrayDeque<int[]> frontier = new ArrayDeque<>(Collections.singletonList(new int[] {0, 0}));
 
         int[] stack = new int[farmNum];
-        ArrayList<Long>[] seenTypes = new ArrayList[farmNum + 1];
-        for (int i = 0; i < farmNum + 1; i++) {
+        ArrayList<Long>[] seenTypes = new ArrayList[farmNum];
+        for (int i = 0; i < farmNum; i++) {
             seenTypes[i] = new ArrayList<>();
         }
         int lastDepth = -1;
@@ -113,32 +86,45 @@ public class MilkVisits {
 
             if (depth != lastDepth + 1) {
                 for (int i = depth; i < lastDepth + 1; i++) {
-                    seenTypes[types[stack[i] - 1]].remove(seenTypes[types[stack[i] - 1]].size() - 1);
+                    seenTypes[types[stack[i]]].remove(seenTypes[types[stack[i]]].size() - 1);
                 }
             }
             stack[depth] = current;
-            seenTypes[types[current - 1]].add(current * MAX_DEPTH + depth);
+            seenTypes[types[current]].add(current * MAX_DEPTH + depth);
 
-            for (int[] got : nodeQueries[current]) {
-                int qNum = got[0];
-                List<Long> theGot = seenTypes[got[1]];
-                if (theGot.size() > 0) {
-                    long haveGotten = theGot.get(theGot.size() - 1);  // see the seenTypes.get()... above for info
-                    int otherFarm = queryLookup[qNum][0] == current ? queryLookup[qNum][1] : queryLookup[qNum][0];
-                    // check if the below one is the other's ancestor- if it isn't, then we'll def have to visit that node on the way to the other one
-                    if (haveGotten / MAX_DEPTH == current || !fatherTest(stack[(int) (haveGotten % MAX_DEPTH + 1)], otherFarm)) {
-                        happiness[qNum] = '1';
-                    }
+            for (int[] q : nodeQueries[current]) {
+                int qNum = q[0];
+                List<Long> ofType = seenTypes[q[1]];
+                if (ofType.size() == 0) {
+                    continue;
+                }
+                long haveGotten = ofType.get(ofType.size() - 1);
+                int otherFarm = queryLookup[qNum][0] == current ? queryLookup[qNum][1] : queryLookup[qNum][0];
+
+                // check if the below one is the other's ancestor- if it isn't, then we'll def have to visit that node on the way to the other one
+                if (haveGotten / MAX_DEPTH == current ||
+                        !fatherTest(stack[(int) (haveGotten % MAX_DEPTH + 1)], otherFarm, inTimes, outTimes)) {
+                    happiness[qNum] = '1';
                 }
             }
 
-            traversed.add(current);
+            traversed[current] = true;
             for (int c : adj[current]) {
-                if (!traversed.contains(c)) {
+                if (!traversed[c]) {
                     frontier.addFirst(new int[] {c, depth + 1});
                 }
             }
             lastDepth = depth;
         }
+
+        PrintWriter written = new PrintWriter("milkvisits.out");
+        written.println(new String(happiness));
+        written.close();
+        System.out.println(new String(happiness));
+        System.out.printf("so we finished: it took something like %s milliseconds%n", System.currentTimeMillis() - start);
+    }
+
+    static boolean fatherTest(int possDad, int possKid, int[] inTimes, int[] outTimes) {
+        return inTimes[possDad] <= inTimes[possKid] && outTimes[possDad] >= outTimes[possKid];
     }
 }
--- official/o2019/dec/silver/silverFriends/MilkVisitsSilver.java
+++ official/o2019/dec/silver/silverFriends/MilkVisitsSilver.java
@@ -1,147 +1,63 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
 
-// 2019 silver december (did this after gold)
+// 2019 dec silver
 public class MilkVisitsSilver {
-    private static final long MAX_DEPTH = 100000;
-    static int farmNum;
-    static int friendNum;
-    static int[] types;
-    static ArrayList<Integer>[] adj;
-    static int[] inTimes;
-    static int[] outTimes;
-    static ArrayList<int[]>[] nodeQueries;
-    static int[][] queryLookup;
-    static char[] happiness;
-
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
         BufferedReader read = new BufferedReader(new FileReader("milkvisits.in"));
         StringTokenizer initial = new StringTokenizer(read.readLine());
-        farmNum = Integer.parseInt(initial.nextToken());
-        friendNum = Integer.parseInt(initial.nextToken());
-        char[] rawTypes = read.readLine().toCharArray();
-        types = new int[farmNum];
-        for (int i = 0; i < farmNum; i++) {
-            types[i] = rawTypes[i] == 'H' ? 1 : 0;
+        int farmNum = Integer.parseInt(initial.nextToken());
+        int friendNum = Integer.parseInt(initial.nextToken());
+        int[] types = read.readLine().chars().map(t -> t - 71).toArray();
+        ArrayList<Integer>[] neighbors = new ArrayList[farmNum];
+        for (int f = 0; f < farmNum; f++) {
+            neighbors[f] = new ArrayList<>();
+        }
+        for (int n = 0; n < farmNum - 1; n++) {
+            int[] path = Arrays.stream(read.readLine().split(" ")).mapToInt(f -> Integer.parseInt(f) - 1).toArray();
+            neighbors[path[0]].add(path[1]);
+            neighbors[path[1]].add(path[0]);
         }
 
-        happiness = new char[friendNum];
-        Arrays.fill(happiness, '0');
-        adj = new ArrayList[farmNum + 1];
-        for (int i = 1; i < farmNum + 1; i++) {  // we won't use index 0 because the farms start @ 1
-            adj[i] = new ArrayList<>();
-        }
-        inTimes = new int[farmNum + 1];  // still, we won't use the first index lol
-        outTimes = new int[farmNum + 1];
-
-        for (int i = 0; i < farmNum - 1; i++) {
-            StringTokenizer path = new StringTokenizer(read.readLine());
-            int firstFarm = Integer.parseInt(path.nextToken());
-            int secondFarm = Integer.parseInt(path.nextToken());
-            adj[firstFarm].add(secondFarm);
-            adj[secondFarm].add(firstFarm);
-        }
-
-        nodeQueries = new ArrayList[farmNum + 1];
-        for (int i = 1; i < farmNum + 1; i++) {
-            nodeQueries[i] = new ArrayList<>();
-        }
-        queryLookup = new int[friendNum][2];
-        for (int fn = 0; fn < friendNum; fn++) {  // farmer john has more friends than i ever will
-            StringTokenizer query = new StringTokenizer(read.readLine());
-            int from = Integer.parseInt(query.nextToken());
-            int to = Integer.parseInt(query.nextToken());
-            int milk = query.nextToken().equals("H") ? 1 : 0;
-            nodeQueries[from].add(new int[] {fn, milk});
-            nodeQueries[to].add(new int[] {fn, milk});
-            queryLookup[fn] = new int[] {from, to};
-        }
-        traverseRecordTimes(1);
-
-        actualSol(1);
-        PrintWriter written = new PrintWriter("milkvisits.out");
-        written.printf("%s%n", new String(happiness));
-        written.close();
-        System.out.println(new String(happiness));
-        System.out.printf("so we finished: it took something like %s milliseconds %n", System.currentTimeMillis() - start);
-    }
-
-    private static void traverseRecordTimes(int startNode) {
-        int movesTaken = 0;
-        HashSet<Integer> traversed = new HashSet<>();
-        LinkedList<Integer> frontier = new LinkedList<>();  // use it for its queue capability
-        frontier.add(startNode);
-
-        while (!frontier.isEmpty()) {
-            int current = frontier.removeFirst();
-            if (traversed.contains(current)) {  // this should be that marker i think
-                outTimes[current] = movesTaken;
+        int[] componentID = new int[farmNum];
+        int soFarID = 1;
+        for (int f = 0; f < farmNum; f++) {
+            if (componentID[f] != 0) {
                 continue;
             }
-
-            inTimes[current] = movesTaken;
-            frontier.addFirst(current);  // set a marker to record the outtime
-            traversed.add(current);
-            for (int c : adj[current]) {
-                if (!traversed.contains(c)) {
-                    frontier.addFirst(c);
-                }
-            }
-            movesTaken++;
-        }
-    }
-
-    private static boolean fatherTest(int possDad, int possKid) {
-        return inTimes[possDad] <= inTimes[possKid] && outTimes[possDad] >= outTimes[possKid];
-    }
-
-    private static void actualSol(int startNode) {
-        HashSet<Integer> traversed = new HashSet<>();
-        LinkedList<int[]> frontier = new LinkedList<>();
-        frontier.add(new int[] {startNode, 0});
-
-        int[] stack = new int[farmNum];
-        ArrayList<Long>[] seenTypes = new ArrayList[farmNum + 1];
-        for (int i = 0; i < farmNum + 1; i++) {
-            seenTypes[i] = new ArrayList<>();
-        }
-        int lastDepth = -1;
-
-        while (!frontier.isEmpty()) {
-            int[] gotten = frontier.removeFirst();
-            int current = gotten[0];
-            int depth = gotten[1];
-
-            if (depth != lastDepth + 1) {
-                for (int i = depth; i < lastDepth + 1; i++) {
-                    seenTypes[types[stack[i] - 1]].remove(seenTypes[types[stack[i] - 1]].size() - 1);
-                }
-            }
-            stack[depth] = current;
-            seenTypes[types[current - 1]].add(current * MAX_DEPTH + depth);
-
-            for (int[] got : nodeQueries[current]) {
-                int qNum = got[0];
-                List<Long> theGot = seenTypes[got[1]];
-                if (theGot.size() > 0) {
-                    long haveGotten = theGot.get(theGot.size() - 1);  // see the seenTypes.get()... above for info
-                    int otherFarm = queryLookup[qNum][0] == current ? queryLookup[qNum][1] : queryLookup[qNum][0];
-                    // check if the below one is the other's ancestor- if it isn't, then we'll def have to visit that node on the way to the other one
-                    if (haveGotten / MAX_DEPTH == current || !fatherTest(stack[(int) (haveGotten % MAX_DEPTH + 1)], otherFarm)) {
-                        happiness[qNum] = '1';
+            componentID[f] = soFarID;
+            int type = types[f];
+            ArrayDeque<Integer> frontier = new ArrayDeque<>(Collections.singletonList(f));
+            while (!frontier.isEmpty()) {
+                int curr = frontier.poll();
+                for (int n : neighbors[curr]) {
+                    if (componentID[n] == 0 && types[n] == type) {
+                        frontier.add(n);
+                        componentID[n] = soFarID;
                     }
                 }
             }
-
-            traversed.add(current);
-            for (int c : adj[current]) {
-                if (!traversed.contains(c)) {
-                    frontier.addFirst(new int[] {c, depth + 1});
-                }
-            }
-            lastDepth = depth;
+            soFarID++;
         }
+
+        PrintWriter written = new PrintWriter("milkvisits.out");
+        for (int f = 0; f < friendNum; f++) {
+            StringTokenizer rawQuery = new StringTokenizer(read.readLine());
+            int farm1 = Integer.parseInt(rawQuery.nextToken()) - 1, farm2 = Integer.parseInt(rawQuery.nextToken()) - 1;
+            int milk = rawQuery.nextToken().charAt(0) - 71;
+            if ((types[farm1] == milk || types[farm2] == milk)
+                    || (componentID[farm1] != componentID[farm2])) {
+                written.write("1");  // have to input it as a string or else java thinks its a char code lol
+                System.out.print(1);
+            } else {
+                written.write("0");
+                System.out.print(0);
+            }
+        }
+        written.println();
+        written.close();
+        System.out.println();
+        System.out.printf("%d ms- bruh moment%n", System.currentTimeMillis() - start);
     }
 }
