3ac9023c2acd60e7f89337fdd7b1293a31660200
SansPapyrus683 <kevinsheng2005@gmail.com>
2021-01-01 22:06:42 -0800

    add some more sols lol

---
+++ src/official/o2013/dec/silver/animalCrossingButCow/Vacation.java
@@ -0,0 +1,75 @@
+package official.o2013.dec.silver.animalCrossingButCow;
+
+import java.io.*;
+import java.util.*;
+
+// 2013 dec silver
+public class Vacation {
+    // i think this is a reasonable upper bound on a singular distance, right?
+    private static final int INVALID = 420696969;
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        BufferedReader read = new BufferedReader(new FileReader("vacation.in"));
+        StringTokenizer initial = new StringTokenizer(read.readLine());
+        int farmNum = Integer.parseInt(initial.nextToken());
+        int flightNum = Integer.parseInt(initial.nextToken());
+        int hubs = Integer.parseInt(initial.nextToken());  // 0 to hubs - 1 are all hubs
+        int queryNum = Integer.parseInt(initial.nextToken());
+        
+        ArrayList<int[]>[] neighbors = new ArrayList[farmNum];
+        for (int f = 0; f < farmNum; f++) {
+            neighbors[f] = new ArrayList<>();
+        }
+        for (int f = 0; f < flightNum; f++) {
+            StringTokenizer flight = new StringTokenizer(read.readLine());
+            neighbors[Integer.parseInt(flight.nextToken()) - 1].add(new int[] {
+                    Integer.parseInt(flight.nextToken()) - 1, // destination
+                    Integer.parseInt(flight.nextToken())  // cost
+                });
+        }
+
+        int[][] distances = new int[farmNum][farmNum];
+        for (int f = 0; f < farmNum; f++) {
+            Arrays.fill(distances[f], INVALID);
+        }
+        // run ez floyd warshall (only 200 farms, so we can do this)
+        for (int f = 0; f < farmNum; f++) {
+            distances[f][f] = 0;
+            for (int[] n : neighbors[f]) {
+                distances[f][n[0]] = n[1];
+            }
+        }
+        for (int i = 0; i < farmNum; i++) {
+            for (int j = 0; j < farmNum; j++) {
+                for (int k = 0; k < farmNum; k++) {
+                    distances[j][k] = Math.min(distances[j][k], distances[j][i] + distances[i][k]);
+                }
+            }
+        }
+
+        int possible = 0;
+        long total = 0;
+        for (int q = 0; q < queryNum; q++) {
+            StringTokenizer query = new StringTokenizer(read.readLine());
+            int from = Integer.parseInt(query.nextToken()) - 1;
+            int to = Integer.parseInt(query.nextToken()) - 1;
+            int minDist = INVALID;
+            for (int h = 0; h < hubs; h++) {  // brute force all possible hubs
+                if (distances[from][h] < INVALID && distances[h][to] < INVALID) {
+                    minDist = Math.min(minDist, distances[from][h] + distances[h][to]);
+                }
+            }
+            if (minDist != INVALID) {
+                total += minDist;
+                possible++;
+            }
+        }
+        read.close();  // just so vscode stops bugging me
+
+        PrintWriter written = new PrintWriter("vacation.out");
+        written.printf("%s%n%s%n", possible, total);
+        written.close();
+        System.out.println(possible + " " + total);
+        System.out.printf("%d ms. ok now HELP I'M TRAPPED IN MY NEIGHBOR'S BASEME-%n", System.currentTimeMillis() - start);
+    }
+}
---
+++ src/official/o2013/dec/silver/boredBessie/Shuffle.java
@@ -0,0 +1,40 @@
+package official.o2013.dec.silver.boredBessie;
+
+import java.io.*;
+import java.util.*;
+
+// 2013 dec silver
+public class Shuffle {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        BufferedReader read = new BufferedReader(new FileReader("shuffle.in"));
+        StringTokenizer initial = new StringTokenizer(read.readLine());
+        // note: cards start w/ 1 at the top and N @ the bottom
+        // but bc i like 0-indexing imma just, ya know, make all the cards 0-indexed
+        int totalCardNum = Integer.parseInt(initial.nextToken());
+        int shuffleCardNum = Integer.parseInt(initial.nextToken());
+        int queryNum = Integer.parseInt(initial.nextToken());
+        int[] reverseShuffle = new int[shuffleCardNum];
+        for (int c = 0; c < shuffleCardNum; c++) {
+            reverseShuffle[Integer.parseInt(read.readLine()) - 1] = c;
+        }
+
+        PrintWriter written = new PrintWriter("shuffle.out");
+        for (int q = 0; q < queryNum; q++) {
+            int query = totalCardNum - Integer.parseInt(read.readLine());
+            int relPos = Math.max(shuffleCardNum - (totalCardNum - query), 0);  // both of these are 0-indexed
+            int absPos = query;
+            int windowStart = Math.min(totalCardNum - shuffleCardNum, absPos);
+            while (relPos != shuffleCardNum && windowStart >= 0) {
+                int nextRelPos = reverseShuffle[relPos];
+                absPos += nextRelPos - relPos;
+                relPos = nextRelPos + 1;
+                windowStart--;
+            }
+            written.println(absPos + 1);
+            System.out.println(absPos + 1);  // turn it back to 1-indexing
+        }
+        written.close();
+        System.out.printf("here it took %d ms are you happy%n", System.currentTimeMillis() - start);
+    }
+}
