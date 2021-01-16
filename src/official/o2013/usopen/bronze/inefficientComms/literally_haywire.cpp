52c663d1c8de4c990709fa873af5503542a1d8ed
SansPapyrus683 <kevinsheng2005@gmail.com>
2021-01-16 11:36:11 -0800

    for no reason at all, did 2019 usopen bronze

---
+++ src/official/o2013/usopen/bronze/inefficientComms/literally_haywire.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+constexpr int FRIEND_NUM = 3;
+
+class WireSystem {
+    private:
+        vector<vector<int>> friendly;
+        vector<int> cow_pos;  // this[i] = the stall number of cow i (everything's 0-indexed)
+        int cow_num;
+        int best = std::numeric_limits<int>::max();
+
+        void calc_min(int arranged_alr, int cost_so_far, int dangling_num, int dangling_cost) {
+            if (arranged_alr == cow_num) {
+                best = std::min(best, cost_so_far);
+                return;
+            }
+            if (cost_so_far + dangling_cost >= best) {
+                return;  // no hope left, we've already exceeded the best amount
+            }
+
+            arranged_alr++;
+            for (int c = 0; c < cow_num; c++) {
+                if (cow_pos[c] != -1) {  // this cow is already assigned, let's leave it alone
+                    continue;
+                }
+                cow_pos[c] = arranged_alr;  // assign it to the empty spot
+
+                int more_dangling = FRIEND_NUM;
+                int to_add = 0;
+                for (int f : friendly[c]) {
+                    if (cow_pos[f] != -1) {  // oh wait, we already assigned this one, let's complete it
+                        to_add += cow_pos[c] - cow_pos[f];
+                        more_dangling -= 2;  // -2 because the friend also had a missing link along with this one
+                    }
+                }
+                calc_min(arranged_alr, 
+                         cost_so_far + to_add, 
+                         dangling_num + more_dangling, 
+                         dangling_cost + (dangling_num + more_dangling) - to_add);
+
+                cow_pos[c] = -1;
+            }
+        }
+    public:
+        WireSystem(vector<vector<int>> friends) {
+            friendly = friends;
+            cow_num = friends.size();
+            cow_pos = vector<int>(cow_num, -1);
+        }
+
+        int min_cost() {
+            calc_min(0, 0, 0, 0);
+            return best;
+        }
+};
+
+/**
+ * 2013 jan silver
+ * ok so because this is a brute force problem
+ * i decided to use c++ because of its speed
+ * if you were looking for a sol in another language i'm sorry
+ */
+int main() {
+    std::ifstream read("haywire.in");
+    int cow_num;
+    read >> cow_num;
+    vector<vector<int>> friends(cow_num, vector<int>(FRIEND_NUM));
+    for (int c = 0; c < cow_num; c++) {
+        for (int f = 0; f < FRIEND_NUM; f++) {
+            read >> friends[c][f];
+            friends[c][f]--;  // make the cows 0-indexed
+        }
+    }
+
+    WireSystem system(friends);
+    int min_cost = system.min_cost();
+
+    std::ofstream written("haywire.out");
+    written << min_cost << endl;
+    cout << min_cost << endl;
+}
---
+++ src/official/o2019/usopen/bronze/sillyFJ/very_flaky.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+// assumes like all the nodes are just from 0 to neighbors.size() - 1
+bool all_reachable(int start, vector<vector<int>> neighbors) {
+    std::deque<int> frontier;
+    frontier.push_back(start);
+    vector<bool> visited(neighbors.size());
+    visited[start] = true;
+    while (!frontier.empty()) {
+        int curr = frontier[0];
+        frontier.pop_front();
+        for (int n : neighbors[curr]) {
+            if (!visited[n]) {
+                visited[n] = true;
+                frontier.push_back(n);
+            }
+        }
+    }
+    return std::find(visited.begin(), visited.end(), false) == visited.end();
+}
+
+// 2019 usopen bronze (screw it, i'm using c++, i need more practice with it anyways)
+int main() {
+    std::ifstream read("factory.in");
+    int station_num;
+    read >> station_num;
+    vector<vector<int>> neighbors(station_num);
+    for (int i = 0; i < station_num - 1; i++) {
+        int from;
+        int to;
+        read >> from >> to;
+        /*
+         * the problem wants a station so that you can get to it from any other one
+         * because that's dumb, we make the edges in reverse and instead 
+         * find a station that can reach any other one
+         */
+        neighbors[to - 1].push_back(from - 1);  // make the stations 0-indexed
+    }
+
+    int valid = -1;
+    for (int s = 0; s < station_num; s++) {
+        if (all_reachable(s, neighbors)) {
+            valid = s + 1;  // convert back to 1-indexing
+            break;
+        }
+    }
+    std::ofstream written("factory.out");
+    written << valid << endl;
+    cout << valid << endl;
+}
