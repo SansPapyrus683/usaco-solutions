d9a54c475e5e35ea37d00fb7de029e0ce8830e4a
SansPapyrus683 <kevinsheng2005@gmail.com>
2021-02-22 09:07:30 -0800

    changed stdin template

--- src/utils/template/STDInTemplate.java
+++ src/utils/template/STDInTemplate.java
@@ -6,10 +6,13 @@ import java.util.*;
 // this is the same thing as template but it uses stdin instead of file input so yeah
 public final class STDInTemplate {
     public static void main(String[] args) throws IOException {
-        long start = System.currentTimeMillis();
+
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("whaddya want me to say? ");
-        System.out.println(read.readLine());
+        String input = read.readLine();
+
+        long start = System.currentTimeMillis();
+        System.out.println(input);
         System.err.printf("this code that took %d ms is absolutely pointless lol%n", System.currentTimeMillis() - start);
     }
 }
