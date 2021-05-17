531f3b34c448edc28ec543007098f47a74ed4d83
Kevin Sheng <kevinsheng2005@gmail.com>
2021-05-16 19:14:26 -0700

    better talent sol (the old actually failed for some cases)

--- src/official/o2018/usopen/gold/fatCows/Talent.java
+++ src/official/o2018/usopen/gold/fatCows/Talent.java
@@ -5,56 +5,64 @@ import java.util.*;
 
 // 2018 usopen gold
 public final class Talent {
-    private static final int INVALID = 420696969;
-    private static final int MUL_BY = 1000;
+    private static final int PRECISION = (int) Math.pow(10, 3);
+    private static class Cow {
+        int weight;
+        int talent;
+        public Cow(int weight, int talent) {
+            this.weight = weight;
+            this.talent = talent;
+        }
+    }
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
         BufferedReader read = new BufferedReader(new FileReader("talent.in"));
         StringTokenizer initial = new StringTokenizer(read.readLine());
         int cowNum = Integer.parseInt(initial.nextToken());
         int weightReq = Integer.parseInt(initial.nextToken());
-        int[][] cows = new int[cowNum][2];
+        Cow[] cows = new Cow[cowNum];
         for (int c = 0; c < cowNum; c++) {
-            // first weight, then talent
-            cows[c] = Arrays.stream(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-        // sauce: https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
-        // bc weight is too large, let's dp on the talent instead
-        int totalTalent = Arrays.stream(cows).mapToInt(c -> c[1]).sum();
-        int[][] minWeights = new int[cowNum + 1][totalTalent + 1];
-        for (int c = 0; c < cowNum; c++) {
-            Arrays.fill(minWeights[c], INVALID);
-        }
-        minWeights[0][0] = 0;
-        for (int c = 1; c <= cowNum; c++) {
-            int[] thisCow = cows[c - 1];
-            for (int t = 0; t <= totalTalent; t++) {
-                if (t == 0) {
-                    minWeights[c][t] = 0;
-                } else if (thisCow[1] <= t) {
-                    minWeights[c][t] = Math.min(
-                            thisCow[0] + minWeights[c - 1][t - thisCow[1]],
-                            minWeights[c - 1][t]
-                    );
-                } else {
-                    minWeights[c][t] = minWeights[c - 1][t];
-                }
-            }
+            int[] cow = Arrays.stream(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            cows[c] = new Cow(cow[0], cow[1]);
         }
 
-        double bestScore = 0;
-        for (int c = 1; c <= cowNum; c++) {
-            for (int t = 0; t <= totalTalent; t++) {
-                if (minWeights[c][t] >= weightReq) {
-                    bestScore = Math.max(bestScore, (double) t / minWeights[c][t]);
-                }
+        long lo = 0;
+        long hi = (long) Arrays.stream(cows).mapToInt(c -> c.talent).sum() * PRECISION;
+        long valid = -1;
+        while (lo <= hi) {
+            long mid = (lo + hi) / 2;
+            if (achievableScore(mid, weightReq, cows)) {
+                lo = mid + 1;
+                valid = mid;
+            } else {
+                hi = mid - 1;
             }
         }
-
         PrintWriter written = new PrintWriter("talent.out");
-        written.println((int) (bestScore * MUL_BY));  // why not just ask for the truncated decimal lol
+        written.println(valid);
         written.close();
-        System.out.println((int) (bestScore * MUL_BY));
+        System.out.println(valid);
         System.out.printf("i swear one day i will find benq and strangle him: %d ms%n", System.currentTimeMillis() - start);
+    }
+
+    // we're going to represent score with an integer to PRECISION
+    private static boolean achievableScore(long score, int weightReq, Cow[] cows) {
+        // so let's define the "leeway" of a set of cows as the talent sum - required score * weight sum
+        double[][] maxLeeway = new double[cows.length + 1][weightReq + 1];
+        Arrays.fill(maxLeeway[0], -Double.MAX_VALUE);  // MIN_VALUE will be when we literally can't achieve weight amt
+        maxLeeway[0][0] = 0;
+        for (int c = 0; c < cows.length; c++) {
+            maxLeeway[c + 1] = maxLeeway[c].clone();
+            Cow nxt = cows[c];
+            for (int w = 0; w <= weightReq; w++) {
+                if (maxLeeway[c][w] == -Double.MAX_VALUE) {
+                    continue;
+                }
+                int newWeight = Math.min(weightReq, w + nxt.weight);
+                double newLeeway = maxLeeway[c][w] + nxt.talent * PRECISION - nxt.weight * score;
+                maxLeeway[c + 1][newWeight] = Math.max(maxLeeway[c + 1][newWeight], newLeeway);
+            }
+        }
+        return maxLeeway[cows.length][weightReq] >= 0.0;
     }
 }
