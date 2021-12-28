31a1d574dc741254daa18e63e5e9fbf7affb3176
SansPapyrus683 <kevinsheng2005@gmail.com>
2021-12-28 14:59:00 -0800

    snake_case

--- src/official/o2013/nov/silver/squishedCows/giant_cows.py
+++ src/official/o2013/nov/silver/squishedCows/giant_cows.py
@@ -6,41 +6,41 @@ from bisect import bisect_left, bisect_right
 
 
 class MaxSegTree:
-    def __init__(self, initLen):
-        self.arrSize = initLen
+    def __init__(self, init_len):
+        self.arr_size = init_len
         self.size = 1
-        while self.size < self.arrSize:
+        while self.size < self.arr_size:
             self.size *= 2
         self.segtree = [0 for _ in range(self.size * 2)]
 
-    def set(self, index: int, val: int, currNode: int = 0, left: int = 0, right: int = -1):
+    def set(self, index: int, val: int, curr_node: int = 0, left: int = 0, right: int = -1):
         right = self.size if right == -1 else right
         if right - left == 1:
-            self.segtree[currNode] = val
+            self.segtree[curr_node] = val
         else:
             mid = (left + right) // 2
             if index < mid:
-                self.set(index, val, 2 * currNode + 1, left, mid)
+                self.set(index, val, 2 * curr_node + 1, left, mid)
             else:
-                self.set(index, val, 2 * currNode + 2, mid, right)
-            self.segtree[currNode] = max(self.segtree[2 * currNode + 1], self.segtree[2 * currNode + 2])
+                self.set(index, val, 2 * curr_node + 2, mid, right)
+            self.segtree[curr_node] = max(self.segtree[2 * curr_node + 1], self.segtree[2 * curr_node + 2])
 
     # start is inclusive, end isn't
-    def max(self, start: int, end: int, currNode: int = 0, left: int = 0, right: int = -1):
+    def max(self, start: int, end: int, curr_node: int = 0, left: int = 0, right: int = -1):
         right = self.size if right == -1 else right
         if right <= start or end <= left:
             return -float('inf')
         if start <= left and right <= end:
-            return self.segtree[currNode]
+            return self.segtree[curr_node]
         mid = (left + right) // 2
-        leftPart = self.max(start, end, 2 * currNode + 1, left, mid)
-        rightPart = self.max(start, end, 2 * currNode + 2, mid, right)
-        return max(leftPart, rightPart)
+        left = self.max(start, end, 2 * curr_node + 1, left, mid)
+        right = self.max(start, end, 2 * curr_node + 2, mid, right)
+        return max(left, right)
 
 
 with open('crowded.in') as read:
-    cowNum, distThreshold = [int(i) for i in read.readline().split()]
-    cows = [[int(i) for i in read.readline().split()] for _ in range(cowNum)]  # position and height
+    cow_num, dist_threshold = [int(i) for i in read.readline().split()]
+    cows = [[int(i) for i in read.readline().split()] for _ in range(cow_num)]  # position and height
 cows.sort()
 positions = [c[0] for c in cows]
 heights = [c[1] for c in cows]
@@ -50,11 +50,11 @@ for v, h in enumerate(heights):
     seg_tree.set(v, h)
 
 total_crowded = 0
-for i in range(1, cowNum - 1):  # the first cow and last cow don't really matter
-    farthest_left = bisect_left(positions, positions[i] - distThreshold)
-    farthest_right = bisect_right(positions, positions[i] + distThreshold) - 1
+for i in range(1, cow_num - 1):  # the first cow and last cow don't really matter
+    farthest_left = bisect_left(positions, positions[i] - dist_threshold)
+    farthest_right = bisect_right(positions, positions[i] + dist_threshold) - 1
     total_crowded += seg_tree.max(i + 1, farthest_right + 1) >= heights[i] * 2 and \
-                    seg_tree.max(farthest_left, i) >= heights[i] * 2
+                     seg_tree.max(farthest_left, i) >= heights[i] * 2
 
 print(total_crowded)
 print(total_crowded, file=open('crowded.out', 'w'))
