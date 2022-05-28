b37029b0ca46daef87717aefc24d67c4c3cfb9bc
SansPapyrus683 <55369003+SansPapyrus683@users.noreply.github.com>
2022-05-28 10:55:34 -0700

    sync

--- src/official/o2016/feb/silver/cowCompass/Balancing.java
+++ src/official/o2016/feb/silver/cowCompass/Balancing.java
@@ -9,7 +9,7 @@ public final class Balancing {
         long start = System.currentTimeMillis();
         BufferedReader read = new BufferedReader(new FileReader("balancing.in"));
         // stringtokenizer for bronze accommodation
-        int cowNum = Integer.parseInt(new StringTokenizer(read.readLine()).nextToken());
+        int cowNum = Integer.parseInt(read.readLine());
         int[][] cows = new int[cowNum][2];
         for (int c = 0; c < cowNum; c++) {
             cows[c] = Arrays.stream(read.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -19,11 +19,14 @@ public final class Balancing {
         }
         Arrays.sort(cows, Comparator.comparingInt(c -> c[0]));
 
+        int[][] byY = cows.clone();
+        Arrays.sort(byY, Comparator.comparingInt(c -> c[1]));
+
         int mostEqual = Integer.MAX_VALUE;
         int vertLineAt = 0;  // these "at" variables are the closest point that's still ahead of the line
         while (vertLineAt < cowNum) {
             int xLine = cows[vertLineAt][0] + 1;
-            mostEqual = Math.min(mostEqual, minPartition(xLine, cows));
+            mostEqual = Math.min(mostEqual, minPartition(xLine, byY));
             while (vertLineAt < cowNum && xLine > cows[vertLineAt][0]) {
                 vertLineAt++;
             }
@@ -36,14 +39,14 @@ public final class Balancing {
         System.out.printf("What did it cost?... %d milliseconds.%n", System.currentTimeMillis() - start);
     }
 
-    // given a vertical line, this find the optimal horizontal line
+    /*
+     * given a vertical line, this find the optimal horizontal line
+     * assumes the cows have been sorted by y-coordinate already
+     */
     private static int minPartition(int xLine, int[][] cows) {
-        int[][] byY = cows.clone();
-        Arrays.sort(byY, Comparator.comparingInt(c -> c[1]));
-
         ArrayList<int[]> leftSide = new ArrayList<>();
         ArrayList<int[]> rightSide = new ArrayList<>();
-        for (int[] c : byY) {
+        for (int[] c : cows) {
             if (c[0] < xLine) {
                 leftSide.add(c);
             } else if (c[0] > xLine) {
@@ -58,8 +61,8 @@ public final class Balancing {
         int rightAt = 0;
         int totalAt = 0;
         while (totalAt < cows.length) {
-            int yLine = byY[totalAt][1] + 1;
-            while (totalAt < cows.length && yLine > byY[totalAt][1]) {
+            int yLine = cows[totalAt][1] + 1;
+            while (totalAt < cows.length && yLine > cows[totalAt][1]) {
                 totalAt++;
             }
             while (leftAt < leftSide.size() && yLine > leftSide.get(leftAt)[1]) {
