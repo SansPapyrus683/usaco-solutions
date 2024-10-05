35cfc2d2952f0b601056782dd03603b0d5f865ca
Kevin Sheng <kevinsheng2005@gmail.com>
2024-10-05 07:30:36 -0700

    better code for walking along a fence
    exploit the stupid bounds a bit more

--- src/official/o2024/usopen/bronze/WalkingAlongFence.java
+++ src/official/o2024/usopen/bronze/WalkingAlongFence.java
@@ -3,12 +3,8 @@ package official.o2024.usopen.bronze;
 import java.awt.Point;
 import java.io.*;
 import java.util.*;
-import java.util.function.IntFunction;
 
-/**
- * 2024 us open bronze
- * this is basically just the silver code copy and pasted, idk what to tell you
- */
+/** 2024 us open bronze */
 public class WalkingAlongFence {
     public static void main(String[] args) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
@@ -51,76 +47,39 @@ public class WalkingAlongFence {
             }
         }
 
-        int[] order = new int[postNum]; // order[0] = 0 by default
-        long[] distPref = new long[postNum + 1];
-        Map<Point, Integer> edgeFirst = new HashMap<>();
-        Map<Point, Integer> postMap = new HashMap<>();
+        Map<Point, Integer> ptInds = new HashMap<>();
+        int last = -1;
         int at = 0;
         for (int p = 0; p < postNum; p++) {
             int moveTo = neighbors[at].get(0);
-            if (p > 0 && moveTo == order[p - 1]) {
+            if (p > 0 && moveTo == last) {
                 moveTo = neighbors[at].get(1);
             }
-            order[p] = at;
-            postMap.put(posts[at], p);
-            edgeFirst.put(new Point(at, moveTo), p);
-            edgeFirst.put(new Point(moveTo, at), p);
-            distPref[p + 1] = distPref[p] + (int) posts[at].distance(posts[moveTo]);
+
+            Point delta =
+                    new Point(
+                            (int) Math.signum(posts[moveTo].x - posts[at].x),
+                            (int) Math.signum(posts[moveTo].y - posts[at].y));
+            Point curr = posts[at];
+            while (!curr.equals(posts[moveTo])) {
+                ptInds.put(curr, ptInds.size());
+                curr = new Point(curr.x + delta.x, curr.y + delta.y);
+            }
+
+            last = at;
             at = moveTo;
         }
 
         for (int c = 0; c < cowNum; c++) {
-            StringTokenizer st = new StringTokenizer(read.readLine());
-            Point[] pos = new Point[2];
-            pos[0] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            pos[1] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            int[][] data = new int[pos.length][];
-            for (int i = 0; i < pos.length; i++) {
-                if (postMap.containsKey(pos[i])) {
-                    data[i] = new int[] {postMap.get(pos[i]), 0};
-                    continue;
-                }
-                if (byX.containsKey(pos[i].x)) {
-                    int ind = largestLt(byX.get(pos[i].x), pos[i].y, p -> posts[p].y);
-                    if (ind % 2 == 0) {
-                        int pt = byX.get(pos[i].x).get(ind);
-                        int ptNext = byX.get(pos[i].x).get(ind + 1);
-                        int first = edgeFirst.get(new Point(pt, ptNext));
-                        data[i] = new int[] {first, (int) pos[i].distance(posts[order[first]])};
-                        continue;
-                    }
-                }
-                int ind = largestLt(byY.get(pos[i].y), pos[i].x, p -> posts[p].x);
-                int pt = byY.get(pos[i].y).get(ind);
-                int ptNext = byY.get(pos[i].y).get(ind + 1);
-                int first = edgeFirst.get(new Point(pt, ptNext));
-                data[i] = new int[] {first, (int) pos[i].distance(posts[order[first]])};
-            }
-            if (Arrays.compare(data[0], data[1]) > 0) {
-                int[] temp = data[0];
-                data[0] = data[1];
-                data[1] = temp;
-            }
-
-            long dist = distPref[data[1][0]] - distPref[data[0][0]] - data[0][1] + data[1][1];
-            System.out.println(Math.min(dist, distPref[postNum] - dist));
+            StringTokenizer cow = new StringTokenizer(read.readLine());
+            int sx = Integer.parseInt(cow.nextToken());
+            int sy = Integer.parseInt(cow.nextToken());
+            int ex = Integer.parseInt(cow.nextToken());
+            int ey = Integer.parseInt(cow.nextToken());
+            int sInd = ptInds.get(new Point(sx, sy));
+            int eInd = ptInds.get(new Point(ex, ey));
+            int dist = Math.abs(eInd - sInd);
+            System.out.println(Math.min(dist, ptInds.size() - dist));
         }
-    }
-
-    // WHY THE HELL DOESN'T JAVE HAVE LOWER BOUND EVEN PYTHON HAS BISECT
-    public static int largestLt(List<Integer> a, int i, IntFunction<Integer> key) {
-        int lo = 0;
-        int hi = a.size() - 1;
-        int valid = -1;
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if (key.apply(a.get(mid)) < i) {
-                valid = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-        return valid;
     }
 }
