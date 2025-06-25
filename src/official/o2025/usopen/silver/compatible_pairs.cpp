698a1a2382ee99f3aee994ac0f9cfa2d630e6a10
Kevin Sheng <55369003+SansPapyrus683@users.noreply.github.com>
2025-06-25 10:18:17 -0700

    Update compatible_pairs.cpp

--- src/official/o2025/usopen/silver/compatible_pairs.cpp
+++ src/official/o2025/usopen/silver/compatible_pairs.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <map>
-#include <set>
 #include <vector>
+#include <algorithm>
+#include <map>
 
 using std::cout;
 using std::endl;
@@ -19,56 +19,42 @@ int main() {
         cows[id] = amt;
     }
 
-    std::set<int> visited;
+    std::map<int, vector<int>> adj;
+    for (const auto& [id, amt] : cows) {
+        if (a - id != id && cows.count(a - id)) {
+            adj[id].push_back(a - id);
+        }
+        if (a != b && b - id != id && cows.count(b - id)) {
+            adj[id].push_back(b - id);
+        }
+    }
+
     long long pairs = 0;
-    for (const auto& [id, amt] : cows) {  // idt second field is used actually
-        if (visited.count(id)) { continue; }
-
-        // this is so, so awful, but i legit don't know how else to get it
-        visited.insert(id);
-        vector<int> frontier{id};
-        int start = -1;
-        while (!frontier.empty()) {
-            int curr = frontier.back();
-            frontier.pop_back();
-            bool has_next = false;
-            for (int t : vector<int>{a, b}) {
-                int next = t - curr;
-                if (next != curr && cows.count(next) && !visited.count(next)) {
-                    visited.insert(next);
-                    frontier.push_back(next);
-                    has_next = true;
-                }
-            }
-            if (!has_next) { start = curr; }
+    // amt isn't used here but idk if underscores are the convention in c++
+    for (const auto& [id, amt] : cows) {
+        if (adj[id].size() != 1) {
+            continue;
         }
-
-        vector<int> in_order{start};
-        bool no_more;
-        do {
-            no_more = true;
-            for (int t : vector<int>{a, b}) {
-                int next = t - in_order.back();
-                if (next != in_order.back() && cows.count(next) &&
-                    (in_order.size() == 1 || next != in_order[in_order.size() - 2])) {
-                    in_order.push_back(next);
-                    no_more = false;
-                }
-            }
-        } while (!no_more);
-
-        for (int i = 0; i < in_order.size() - 1; i++) {
-            int use = std::min(cows[in_order[i]], cows[in_order[i + 1]]);
-            cows[in_order[i]] -= use;
-            cows[in_order[i + 1]] -= use;
+        int prev = id;
+        int at = adj[id][0];
+        while (adj[at].size() == 2) {
+            int use = std::min(cows[prev], cows[at]);
             pairs += use;
+            cows[prev] -= use;
+            cows[at] -= use;
+            int old_at = at;
+            at = adj[at][0] == prev ? adj[at][1] : adj[at][0];
+            prev = old_at;
         }
-        for (int i : vector<int>{in_order[0], in_order.back()}) {
-            if (i + i == a || i + i == b) {
-                int use = cows[i] / 2;
-                cows[i] -= use * 2;
-                pairs += use;
-            }
+
+        int use = std::min(cows[prev], cows[at]);
+        pairs += use;
+        cows[prev] -= use;
+        cows[at] -= use;
+    }
+    for (const auto& [id, amt] : cows) {
+        if (id + id == a || id + id == b) {
+            pairs += amt / 2;
         }
     }
 
